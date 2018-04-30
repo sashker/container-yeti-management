9f0f5e85ee37f8dc4c86127777e34fad0959fb54
Alexander Mustafin <mustafin.aleksandr@gmail.com>
2018-04-30 17:54:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#yeti-management in a container
+#### Containerized version of yeti-management daemon (part of the "Yeti switch" project)
+We are aimed to contenerize yeti-management in order to provide an opportunity to run it
+in Docker and other container services.
+
+##Version notification
+Please be informed that versions of our containers might be different from versions of included services 
+(e.g. the version of yeti-manamegent is 1.0.20 but the version of container is 1.0.1)
---
+++ images/docker/build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -l
+
+set -e
+
+# Use local cache proxy if it can be reached, else nothing.
+eval $(detect-proxy enable)
+
+log::m-info "We have to create user $USER"
+build::user::create $USER
+
+log::m-info "Installing essentials ..."
+apt-get update -qq
+apt-get install -yqq \
+    ca-certificates \
+	iputils-ping \
+    libprotobuf-dev \
+    libnanomsg-dev \
+    libconfuse-dev \
+    build-essential \
+    cmake \
+    protobuf-compiler \
+    pkg-config \
+    git
+
+log::m-info "Creating the config directory ..."
+mkdir /etc/yeti
+
+log::m-info "Cloning the project repo ..."
+cd /tmp
+git clone -b $BRANCH $REPO
+
+log::m-info "Making the necessary build directory ..."
+cd yeti-management
+mkdir build && cd build
+
+log::m-info "Compiling the project ..."
+cmake ..
+
+log::m-info "Installing files of the project ..."
+make install
+
+log::m-info "Removing jq and git ..."
+apt-get purge -y --auto-remove git build-essential cmake protobuf-compiler pkg-config
+
+log::m-info "Cleaning up ..."
+apt-clean --aggressive
+rm -rf /tmp/yeti-management
+
+# if applicable, clean up after detect-proxy enable
+eval $(detect-proxy disable)
+
+rm -r -- "$0"
